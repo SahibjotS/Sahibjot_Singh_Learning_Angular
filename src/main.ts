ecfbdaac4195b29825c387796d8ad2bff4a69549
SahibjotS <W0835531@myscc.ca>
2024-10-12 20:18:28 -0400

    create the component and provide router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter,Routes} from "@angular/router";
+import  {LaptopListComponent} from "./app/laptop-list/laptop-list.component";
+import {LaptopListItemComponent} from "./app/laptop-list-item/laptop-list-item.component";
 
+const routes:Routes=[
+  {path:'',redirectTo:'/laptop',pathMatch:'full'},
+  {path:'laptop',component: LaptopListComponent},
+  {path:'laptop/:id',component:LaptopListItemComponent}
+]
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
